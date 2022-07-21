9904a6ec01ddfe8910dafac83caafd43710c5212
valeri zanguri <99758290+zanguraa@users.noreply.github.com>
2022-07-21 22:22:42 +0400

    Update 3 task.js

--- 3 task.js
+++ 3 task.js
@@ -1,3 +1,8 @@
+//task 3.
+//Write a function that takes the base and height of a triangle and return its area.
+
+
+//my function:
 function triangle (base, height) {
     let sumTriangle = base * height / 2;
     return sumTriangle;
