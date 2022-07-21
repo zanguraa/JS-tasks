73c5a3efdb360adf9d616a1da236f5c39e0f9e5d
valeri zanguri <99758290+zanguraa@users.noreply.github.com>
2022-07-21 23:04:37 +0400

    create task 7

---
+++ 7 task.js
@@ -0,0 +1,10 @@
+//Create a function that takes an array containing only numbers and return the first element.
+
+//my function:
+
+function getFirstValue(array){
+    let firstEl = array[0];
+    return firstEl;
+}
+
+console.log(getFirstValue([80, 5, 100]));
