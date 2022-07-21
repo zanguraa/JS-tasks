b2245754359154eb7985f49ed7544e59c2e5cc6c
valeri zanguri <99758290+zanguraa@users.noreply.github.com>
2022-07-21 22:21:15 +0400

    Update 4 task.js

--- 4 task.js
+++ 4 task.js
@@ -1,3 +1,7 @@
+//task 4.
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+//my function:
 function addition(n) {
     let number = n + 1;
     return number;
