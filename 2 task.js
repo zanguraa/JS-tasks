6b800d3edf9c528f469a08269af1ee4d7a861470
valeri zanguri <99758290+zanguraa@users.noreply.github.com>
2022-07-21 21:46:40 +0400

    Update 2 task.js

--- 2 task.js
+++ 2 task.js
@@ -1,7 +1,17 @@
+//There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+
+Examples
+remainder(1, 3) ➞ 1 
+remainder(3, 4) ➞ 3
+remainder(-9, 45) ➞ -9 
+remainder(5, 5) ➞ 0
+/////////////////////////////////
+
 function percent (oneN, twoN) {
     let sum = oneN % twoN;
     return sum;
 }
 
-percent(10, 5);
+
 console.log(percent(10, 5));
